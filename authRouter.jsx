88fd5619015145fbdcabac6e886a56ffa69fb827
Paparn Hatnumsap <paparn@thevcgroup.com>
2020-08-05 16:56:24 +0700

    Initial

---
+++ authRouter.jsx
@@ -0,0 +1,28 @@
+import Path from "../../constance/paths"
+import React from "react";
+import BaseRouter from "../../components/baseRouter";
+import {Label} from 'semantic-ui-react'
+
+
+class AuthRouter extends BaseRouter {
+
+    renderLoginForm = () => { return <Label>Login</Label>};
+    renderRegisterForm = () => { return <Label>Register</Label>};
+
+    routes = [
+        {
+            'key': 'login_form',
+            'path': '/login',
+            'exact': false,
+            'render': this.renderLoginForm
+        },
+        {
+            'key': 'register_form',
+            'path': '/register',
+            'exact': false,
+            'render': this.renderRegisterForm
+        },
+    ];
+}
+
+export default AuthRouter;
